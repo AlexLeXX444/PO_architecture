1b48a418191bdd927dcf6eb3da0092cd306e45d6
AlexLeXX <alexlexx444@yandex.ru>
2022-12-26 12:50:32 +0500

    add circle

---
+++ Work_3/geometricShapes/src/main/java/org/example/shapes/CircleShape.java
@@ -0,0 +1,34 @@
+package org.example.shapes;
+
+import org.example.exceptions.IsExistException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CircleShape extends BaseShape{
+
+    public CircleShape(double radius) throws IsExistException {
+        if (isExist(radius)) {
+            setShapeType("circle");
+            List<Double> list = new ArrayList<>();
+            list.add(radius);
+            setShapeSides(list);
+        } else {
+            throw new IsExistException();
+        }
+    }
+
+    private boolean isExist(double a) {
+        return a > 0;
+    }
+
+    @Override
+    public double solvePerimeter() {
+        return 2 * Math.PI * shapeSides.get(0);
+    }
+
+    @Override
+    public double solveArea() {
+        return Math.PI * Math.pow(shapeSides.get(0), 2);
+    }
+}
